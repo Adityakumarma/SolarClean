7060fa47d5bb0baf80502a8fb2798ff26f9bd150
Adityakumarma <adityakumarma@gmail.com>
2026-05-08 15:43:12 +0530

    update

--- src/Pages/Client.jsx
+++ src/Pages/Client.jsx
@@ -3,6 +3,19 @@ import axios from "axios";
 import Swal from 'sweetalert2'
 import { MapContainer, TileLayer, Marker, Popup, LayersControl, useMapEvents } from "react-leaflet";
 import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 
 // const API = "http://localhost:5000/api";
